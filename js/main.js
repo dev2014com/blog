a7d47badc30dc2856949133f0126a0786ca62fef
dev2014com <fengsu@live.com>
2015-01-22 20:57:07 +0800

    xxx

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/**
+ * Created by root on 15-1-22.
+ */
+function nav(obj) {
+    var objName = obj.value;
+    if (objName == "Home") {
+        window.open("http://blog.dev2014.com/");
+    }
+    if (objName == "Hardware") {
+        window.open("hardware.html");
+    }
+    if (objName == "Software") {
+        window.open("software.html");
+    }
+    if (objName == "Album") {
+        window.open("http://www.dev2014.com/album.html");
+    }
+    if (objName == "Music") {
+        window.open("http://www.dev2014.com/music.html");
+    }
+}
